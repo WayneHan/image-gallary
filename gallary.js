57cc1bac1e649332824f323f9b365cf00348e4ed
Wayne <1830261482@qq.com>
2016-08-26 22:38:26 +0800

    image-gallary

--- gallary.js
+++ gallary.js
@@ -1,11 +1,11 @@
-function getpic(pic){
+function getpic(pic) {
     var source = pic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
 }
 
-function countBodyChildren(){
+function countBodyChildren() {
     var body_element = document.getElementsByTagName("body")[0];
     alert(body_element.childNodes.length);
-    window.onload = countBodyChildren;
 }
+window.onload = countBodyChildren;
