db66ba8ddcb1f3fdb4b252662de57270e9753f82
Wayne <1830261482@qq.com>
2016-08-26 16:04:09 +0800

    gallary

---
+++ test.js
@@ -0,0 +1,10 @@
+/*var paras = document.getElementsByTagName("p")
+for(var i = 0; i < paras.length; i++){
+    var title_text = paras[i].getAttribute("title");
+    if(title_text){
+        alert(title_text);
+    }
+}*/
+
+var shopping = document.getElementById("purchases");
+shopping.setAttribute("title", "a list of goods");
