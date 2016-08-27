8fa6130d80834cebb74d9747feae46defc09ce37
Wayne <1830261482@qq.com>
2016-08-27 16:42:29 +0800

    image-gallary

---
+++ ajax/addLoadEvent.js
@@ -0,0 +1,12 @@
+function addloadevent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function'){
+		window.onload = func;
+	}
+	else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
